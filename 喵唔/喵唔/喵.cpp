d5f304e718f55450ea0107ddb81bd26b915082ef
wam730 <62891920+wam730@users.noreply.github.com>
2020-08-03 13:56:28 +0800

    Updated in 2020.8.3

---
+++ 喵唔/喵唔/喵.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define ίχίχ main
+#define ίχίχίχ (
+#define ίχίχίν )
+#define ίχ "\n"
+#define ίχίνίχίν "I Love You"
+#define ίχίνίν return
+#define ίνίνίχ 0
+#define ίχίχίνίν int 
+#define ίχίνίχ {
+#define ίνίχίν }
+#define ίν ;
+#define ίχίχίχίχίχ printf 
+ίχίχίνίν ίχίχ ίχίχίχ ίχίχίν
+ίχίνίχ
+ίχίχίχίχίχ ίχίχίχ ίχίνίχίν ίχίχίν ίν ίχ ίν
+ίχίνίν ίνίνίχ ίν
+ίνίχίν
